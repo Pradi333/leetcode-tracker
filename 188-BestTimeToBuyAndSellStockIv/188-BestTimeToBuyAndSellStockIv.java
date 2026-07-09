530fb51a17e67653485e36a9da9309034d703534
Pradi333 <24243071@drngpit.ac.in>
2026-07-09 10:06:20 +0530

    Create 188-BestTimeToBuyAndSellStockIv

---
+++ 188-BestTimeToBuyAndSellStockIv/188-BestTimeToBuyAndSellStockIv.java
@@ -0,0 +1,36 @@
+// Last updated: 7/9/2026, 10:06:20 AM
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+
+        int n = prices.length;
+        if (n == 0) return 0;
+
+        // If k is large → same as unlimited transactions
+        if (k >= n / 2) {
+            int profit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    profit += prices[i] - prices[i - 1];
+                }
+            }
+            return profit;
+        }
+
+        int[] buy = new int[k + 1];
+        int[] sell = new int[k + 1];
+
+        for (int i = 0; i <= k; i++) {
+            buy[i] = Integer.MIN_VALUE;
+        }
+
+        for (int price : prices) {
+            for (int t = 1; t <= k; t++) {
+                
+                buy[t] = Math.max(buy[t], sell[t - 1] - price);
+                sell[t] = Math.max(sell[t], buy[t] + price);
+            }
+        }
+
+        return sell[k];
+    }
+}
