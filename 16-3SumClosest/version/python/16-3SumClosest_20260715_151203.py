18664a5a390f911db90b546f13e918b691968522
Pradi333 <24243071@drngpit.ac.in>
2026-07-15 15:12:03 +0530

    Create 16-3SumClosest

---
+++ 16-3SumClosest/version/python/16-3SumClosest_20260715_151203.py
@@ -0,0 +1,29 @@
+# Last updated: 7/15/2026, 3:12:03 PM
+1class Solution(object):
+2    def threeSumClosest(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: int
+7        """
+8        nums.sort()
+9        closest = nums[0] + nums[1] + nums[2]
+10
+11        for i in range(len(nums) - 2):
+12            left = i + 1
+13            right = len(nums) - 1
+14
+15            while left < right:
+16                total = nums[i] + nums[left] + nums[right]
+17
+18                if abs(target - total) < abs(target - closest):
+19                    closest = total
+20
+21                if total < target:
+22                    left += 1
+23                elif total > target:
+24                    right -= 1
+25                else:
+26                    return total
+27
+28        return closest
