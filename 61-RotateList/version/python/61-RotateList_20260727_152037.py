e4bf762485ad0da976af1bca0f022ede32c40fd6
Pradi333 <24243071@drngpit.ac.in>
2026-07-27 15:20:36 +0530

    Create 61-RotateList

---
+++ 61-RotateList/version/python/61-RotateList_20260727_152037.py
@@ -0,0 +1,10 @@
+# Last updated: 7/27/2026, 3:20:37 PM
+1class Solution:
+2    def uniquePaths(self, m, n):
+3        dp = [1] * n
+4
+5        for i in range(1, m):
+6            for j in range(1, n):
+7                dp[j] = dp[j] + dp[j - 1]
+8
+9        return dp[n - 1]
