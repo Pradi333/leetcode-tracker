e76b88959e911772350f6318bff720c85654c194
Pradi333 <24243071@drngpit.ac.in>
2026-07-28 16:00:03 +0530

    Create 76-MinimumWindowSubstring

---
+++ 76-MinimumWindowSubstring/version/python/76-MinimumWindowSubstring_20260728_160004.py
@@ -0,0 +1,18 @@
+# Last updated: 7/28/2026, 4:00:04 PM
+1class Solution:
+2    def subsets(self, nums):
+3        result = []
+4
+5        def backtrack(start, current):
+6            result.append(current[:])
+7
+8            for i in range(start, len(nums)):
+9                current.append(nums[i])
+10
+11                backtrack(i + 1, current)
+12
+13                current.pop()
+14
+15        backtrack(0, [])
+16
+17        return result
