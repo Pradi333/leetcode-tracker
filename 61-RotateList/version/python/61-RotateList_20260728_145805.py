229f7c149a23d28096cf35d9039048874572e8d2
Pradi333 <24243071@drngpit.ac.in>
2026-07-28 14:58:04 +0530

    Create 61-RotateList

---
+++ 61-RotateList/version/python/61-RotateList_20260728_145805.py
@@ -0,0 +1,19 @@
+# Last updated: 7/28/2026, 2:58:05 PM
+1class Solution:
+2    def mySqrt(self, x):
+3        left = 0
+4        right = x
+5
+6        while left <= right:
+7            mid = (left + right) // 2
+8
+9            if mid * mid == x:
+10                return mid
+11
+12            elif mid * mid < x:
+13                left = mid + 1
+14
+15            else:
+16                right = mid - 1
+17
+18        return right
