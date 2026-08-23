c70ecdb85cbecfbd6fe1ca39af833d7c3ed1f2e8
Pradi333 <24243071@drngpit.ac.in>
2026-08-23 08:51:07 +0530

    Create

---
+++ problem-1787455267740/version/python/problem-1787455267740_20260823_085107.py
@@ -0,0 +1,21 @@
+# Last updated: 8/23/2026, 8:51:07 AM
+1class Solution(object):
+2    def findDisappearedNumbers(self, nums, lower, upper):
+3        nums=sorted(set(nums))
+4        result=[]
+5        prev=lower
+6        for x in nums:
+7            if x<lower:
+8                continue
+9            if x>upper:
+10                break
+11            if prev<x:
+12                result.append([prev,x-1])
+13            prev =x+1
+14        if prev<=upper:
+15            result.append([prev,upper])
+16        return result
+17        
+18    
+19                    
+20        
